22b1835df35205d5e20b74dc2187c653a7fb708d
roman <romandragunov@gmail.com>
2023-11-30 15:10:26 +0700

    api service

--- app/src/main/java/ru/aeon/testapp/data/remote/service/ApiService.kt
+++ app/src/main/java/ru/aeon/testapp/data/remote/service/ApiService.kt
@@ -2,12 +2,13 @@ package ru.aeon.testapp.data.remote.service
 
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.Field
 import retrofit2.http.POST
 import ru.aeon.testapp.data.remote.dto.auth.LoginRequestDto
+import ru.aeon.testapp.data.remote.dto.auth.LoginResponseDto
+import ru.aeon.testapp.data.remote.dto.base.ResponseDto
 
 interface ApiService {
     
     @POST("/login")
-    suspend fun requestAuthPhoneCode(@Body loginRequest: LoginRequestDto): Response<String>
+    suspend fun requestAuthPhoneCode(@Body loginRequest: LoginRequestDto): Response<ResponseDto<LoginResponseDto>>
 }
