fc0494ea1cb3f764340af341b9bd9779798e5752
roman <romandragunov@gmail.com>
2023-11-30 20:13:06 +0700

    login navigation

--- app/src/main/java/ru/aeon/testapp/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/ru/aeon/testapp/presentation/ui/main/MainActivity.kt
@@ -2,18 +2,29 @@ package ru.aeon.testapp.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import ru.aeon.testapp.R
 import ru.aeon.testapp.databinding.ActivityMainBinding
+import ru.aeon.testapp.domain.usecase.auth.IsLoggedInUseCase
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     
     private val binding by viewBinding { ActivityMainBinding.inflate(layoutInflater) }
     
+    @Inject
+    internal lateinit var isLoggedIn: IsLoggedInUseCase
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    
+        val graph = if (isLoggedIn()) R.navigation.payments else R.navigation.auth
+        val navHost = binding.navHostFragmentContentMain.getFragment<NavHostFragment>()
+        navHost.navController.setGraph(graph)
+    
         setContentView(binding.root)
     }
 }
