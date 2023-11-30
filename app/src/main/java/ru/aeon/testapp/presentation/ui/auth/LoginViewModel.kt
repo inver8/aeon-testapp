d623875009d1851a208d64fadb3f6cca6747f6c8
roman <romandragunov@gmail.com>
2023-11-30 15:10:56 +0700

    login

--- app/src/main/java/ru/aeon/testapp/presentation/ui/auth/LoginViewModel.kt
+++ app/src/main/java/ru/aeon/testapp/presentation/ui/auth/LoginViewModel.kt
@@ -1,12 +1,13 @@
 package ru.aeon.testapp.presentation.ui.auth
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    
+    private val savedStateHandle: SavedStateHandle,
 ): ViewModel() {
     
     
